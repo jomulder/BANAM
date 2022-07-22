912cc8dec5e9c8d7b6423bd0ae62fbfbc95f21d7
jomulder <jomulder@gmail.com>
2022-07-22 21:55:08 +0200

    updeet data

--- R/W_birds.R
+++ R/W_birds.R
@@ -1,125 +1,10 @@
 #'  @title Weight matrix for threatened bird data
 #'
-#'  @description Data from a study on spatial effect on country's threatened bird
+#'  @description Generated data from a study on spatial effect on country's threatened bird
 #'  rates by McPherson and Nieswiadomy (2005). This data set stores
 #'  the data on the weighted shared border length of 113 neighboring
 #'  countries in the year 2000.
 #'
-#'  \tabular{lll}{
-#'     \strong{Algeria} \tab \code{numeric} \cr
-#'    \strong{Angola} \tab \code{numeric} \cr
-#'    \strong{Argentina} \tab \code{numeric} \cr
-#'    \strong{Australia} \tab \code{numeric} \cr
-#'    \strong{Austria} \tab \code{numeric} \cr
-#'    \strong{Hungary} \tab \code{numeric} \cr
-#'    \strong{Bhutan} \tab \code{numeric} \cr
-#'    \strong{Bahamas} \tab \code{numeric} \cr
-#'    \strong{Belgium} \tab \code{numeric} \cr
-#'    \strong{Bolivia} \tab \code{numeric} \cr
-#'    \strong{Botswana} \tab \code{numeric} \cr
-#'    \strong{Brazil} \tab \code{numeric} \cr
-#'    \strong{Bulgaria} \tab \code{numeric} \cr
-#'    \strong{Burundi} \tab \code{numeric} \cr
-#'    \strong{Cameroon} \tab \code{numeric} \cr
-#'    \strong{Canada} \tab \code{numeric} \cr
-#'    \strong{CAR} \tab \code{numeric} \cr
-#'    \strong{Sri Lanka} \tab \code{numeric} \cr
-#'    \strong{Chad} \tab \code{numeric} \cr
-#'    \strong{Chile} \tab \code{numeric} \cr
-#'    \strong{China PR} \tab \code{numeric} \cr
-#'    \strong{Colombia} \tab \code{numeric} \cr
-#'    \strong{Congo} \tab \code{numeric} \cr
-#'    \strong{Zaire} \tab \code{numeric} \cr
-#'    \strong{Costa Rica} \tab \code{numeric} \cr
-#'    \strong{Benin} \tab \code{numeric} \cr
-#'    \strong{Denmark} \tab \code{numeric} \cr
-#'    \strong{Ecuador} \tab \code{numeric} \cr
-#'    \strong{El Salvador} \tab \code{numeric} \cr
-#'    \strong{Equa Guinea} \tab \code{numeric} \cr
-#'    \strong{Ethiopia} \tab \code{numeric} \cr
-#'    \strong{Finland} \tab \code{numeric} \cr
-#'    \strong{France} \tab \code{numeric} \cr
-#'    \strong{Gabon} \tab \code{numeric} \cr
-#'    \strong{Gambia} \tab \code{numeric} \cr
-#'    \strong{Ghana} \tab \code{numeric} \cr
-#'    \strong{Greece} \tab \code{numeric} \cr
-#'    \strong{Guatemala} \tab \code{numeric} \cr
-#'    \strong{Guinea} \tab \code{numeric} \cr
-#'    \strong{Guinea-B'au} \tab \code{numeric} \cr
-#'    \strong{Guyana} \tab \code{numeric} \cr
-#'    \strong{Hispaniola} \tab \code{numeric} \cr
-#'    \strong{Honduras} \tab \code{numeric} \cr
-#'    \strong{Iceland} \tab \code{numeric} \cr
-#'    \strong{India} \tab \code{numeric} \cr
-#'    \strong{Indonesia} \tab \code{numeric} \cr
-#'    \strong{Iran} \tab \code{numeric} \cr
-#'    \strong{Israel} \tab \code{numeric} \cr
-#'    \strong{Italy} \tab \code{numeric} \cr
-#'    \strong{Cote D'Ivoire} \tab \code{numeric} \cr
-#'    \strong{Jamaica} \tab \code{numeric} \cr
-#'    \strong{Japan} \tab \code{numeric} \cr
-#'    \strong{Kenya} \tab \code{numeric} \cr
-#'    \strong{Korea Rep} \tab \code{numeric} \cr
-#'    \strong{Kuwait} \tab \code{numeric} \cr
-#'    \strong{Laos} \tab \code{numeric} \cr
-#'    \strong{Lebanon} \tab \code{numeric} \cr
-#'    \strong{Lesotho} \tab \code{numeric} \cr
-#'    \strong{Madagascar} \tab \code{numeric} \cr
-#'    \strong{Malawi} \tab \code{numeric} \cr
-#'    \strong{Malaysia} \tab \code{numeric} \cr
-#'    \strong{Mali} \tab \code{numeric} \cr
-#'    \strong{Mauritania} \tab \code{numeric} \cr
-#'    \strong{Mexico} \tab \code{numeric} \cr
-#'    \strong{Mongolia} \tab \code{numeric} \cr
-#'    \strong{Morocco} \tab \code{numeric} \cr
-#'    \strong{Mozambique} \tab \code{numeric} \cr
-#'    \strong{Namibia} \tab \code{numeric} \cr
-#'    \strong{Nepal} \tab \code{numeric} \cr
-#'    \strong{Netherlands} \tab \code{numeric} \cr
-#'    \strong{New Zealand} \tab \code{numeric} \cr
-#'    \strong{Nicaragua} \tab \code{numeric} \cr
-#'    \strong{Niger} \tab \code{numeric} \cr
-#'    \strong{Nigeria} \tab \code{numeric} \cr
-#'    \strong{Oman} \tab \code{numeric} \cr
-#'    \strong{Pakistan} \tab \code{numeric} \cr
-#'    \strong{Bangladesh} \tab \code{numeric} \cr
-#'    \strong{Panama} \tab \code{numeric} \cr
-#'    \strong{Papua New Guinea} \tab \code{numeric} \cr
-#'    \strong{Paraguay} \tab \code{numeric} \cr
-#'    \strong{Peru} \tab \code{numeric} \cr
-#'    \strong{Philippines} \tab \code{numeric} \cr
-#'    \strong{Poland} \tab \code{numeric} \cr
-#'    \strong{Rwanda} \tab \code{numeric} \cr
-#'    \strong{Saudi Arabia} \tab \code{numeric} \cr
-#'    \strong{Senegal} \tab \code{numeric} \cr
-#'    \strong{Sierra Leone} \tab \code{numeric} \cr
-#'    \strong{So Africa} \tab \code{numeric} \cr
-#'    \strong{Spain} \tab \code{numeric} \cr
-#'    \strong{Sudan} \tab \code{numeric} \cr
-#'    \strong{Suriname} \tab \code{numeric} \cr
-#'    \strong{Swaziland} \tab \code{numeric} \cr
-#'    \strong{Norway} \tab \code{numeric} \cr
-#'    \strong{Sweden} \tab \code{numeric} \cr
-#'    \strong{Switzerland} \tab \code{numeric} \cr
-#'    \strong{Tanzania} \tab \code{numeric} \cr
-#'    \strong{Thailand} \tab \code{numeric} \cr
-#'    \strong{Togo} \tab \code{numeric} \cr
-#'    \strong{Trinidad} \tab \code{numeric} \cr
-#'    \strong{Tunisia} \tab \code{numeric} \cr
-#'    \strong{Turkey} \tab \code{numeric} \cr
-#'    \strong{Uganda} \tab \code{numeric} \cr
-#'    \strong{UAE} \tab \code{numeric} \cr
-#'    \strong{Egypt} \tab \code{numeric} \cr
-#'    \strong{UK} \tab \code{numeric} \cr
-#'    \strong{Ireland} \tab \code{numeric} \cr
-#'    \strong{Zimbabwe} \tab \code{numeric} \cr
-#'    \strong{US} \tab \code{numeric} \cr
-#'    \strong{Burkina Faso} \tab \code{numeric} \cr
-#'    \strong{Uruguay} \tab \code{numeric} \cr
-#'    \strong{Venezuela} \tab \code{numeric} \cr
-#'    \strong{Vietnam} \tab \code{numeric} \cr
-#'    \strong{Zambia} \tab \code{numeric} \cr
-#'  }
 #'  @docType data
 #'  @keywords datasets
 #'  @name W_birds
@@ -127,5 +12,5 @@
 #'  @references McPherson, M. A., & Nieswiadomy, M. L. (2005).
 #'  Environmental Kuznets curve: threatened species and spatial
 #'  effects. Ecological Economics, 55(3), 395-407.
-#'  @format A data.frame with 113 rows amd 113 columns
+#'  @format A data.frame with 113 rows and 113 columns
 ##
